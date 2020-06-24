afb1e16a7989ca078339fbe37073421c2b16a545
Rogério Tavares <rogerio.tavares@inovahu.com>
2020-06-24 14:36:48 -0300

    função divisão test

---
+++ src/functions.divisao.test.js
@@ -0,0 +1,21 @@
+const { divisao } = require('./functions');
+
+describe('Divisão', () => {
+  it('Deve dividir inteiros', () => {
+    expect(divisao(3,2)).toEqual(1.5);
+    expect(divisao(10,-1)).toEqual(-10);
+    expect(divisao(-10,-5)).toBe(2);
+    expect(divisao(1,-2)).toBe(-0.5);
+  });
+
+  it('Deve retornar null se algum parâmetro não for definido/null', () => {
+    expect(divisao(undefined,2)).toBeNull();
+    expect(divisao(1,undefined)).toBeNull();
+    expect(divisao(null,2)).toBeNull();
+    expect(divisao(1,null)).toBeNull();
+  });
+
+  it('Deve retornar null se o parametro "b" for zero', () => {
+    expect(divisao(1,0)).toBeNull();
+  });
+});
